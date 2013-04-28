0471641d143d586d81c2f9a6ea3b5b7139b53167
NimmiCV <nimmicv@gmail.com>
2013-04-28 00:19:55 -0700

    updating Visualization..3

--- visualization/ui.r
+++ visualization/ui.r
@@ -1,13 +1,20 @@
 library(shiny)
 
 # Define UI for application that plots random distributions 
-shinyUI(pageWithSidebar(
+shinyUI(div(style="background-image: url('http://2.bp.blogspot.com/_oigT0GOX_oY/TUaTLM1xBlI/AAAAAAAAAXs/i6atDqKiSM0/s1600/The-best-top-desktop-blue-wallpapers-blue-wallpaper-blue-background-hd-6.jpg')",pageWithSidebar(
   
   # Application title
   headerPanel("Visualize Data of Santa Clara County !"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
+    tags$head(
+      tags$style(type="text/css", "select { max-width: 200px; }"),
+      tags$style(type="text/css", "textarea { max-width: 185px; }"),
+      tags$style(type="text/css", ".jslider { max-width: 200px; }"),
+      tags$style(type='text/css', ".well { max-width: 310px;background-color: re }"),
+      tags$style(type='text/css', ".span4 { max-width: 310px; }")
+    ),
     selectInput("city1", 
                 "Select City 1",
                 list(
@@ -47,4 +54,4 @@ shinyUI(pageWithSidebar(
     htmlOutput("gvis")
    
   )
-))
+)))
