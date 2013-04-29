acbb53cf2f0a9d5a4e4e91a6b448cf8f43c24e12
NimmiCV <nimmicv@gmail.com>
2013-04-29 00:55:30 -0700

    Updating Visualization with Prediction

--- visualization/server.R
+++ visualization/server.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(googleVis)
+library(datasets)
 
-SantaClaraCountyCrimeData3 <- read.csv("/Users/manu/Desktop/MyData/SantaClaraCountyCrimeData3.csv")
+SantaClaraCountyCrimeData3 <- read.csv("SantaClaraCountyCrimeData3.csv")
 Gilroy <- read.csv("/Users/manu/Desktop/MyData/Gilroy.csv")
 
 shinyServer(function(input,output){
@@ -17,6 +18,37 @@ shinyServer(function(input,output){
   data2<-SantaClaraCountyCrimeData3[SantaClaraCountyCrimeData3$City==input$city2,]
   data<-rbind(data1,data2)
   gvisMotionChart(data,idvar="City",timevar="Year")
+  })
+  output$help <- renderText({formulaText()})
+  output$basePlot <-renderPlot({
+    plot(SantaClaraCountyCrimeData3)
+  })
+  
+  output$gvis1 <- renderGvis({
+    SantaClaraData2 <- read.csv("SantaClaraCountyCrimeData3.csv")
+    test.frame <- SantaClaraData2 [, 1:5]
+    mod1 <- lm(Population ~ Year + City, data=test.frame) 
+    mod2 <- lm(propertyCrime~Year+City, data=test.frame)
+    mod3 <- lm(UnemploymentRate ~ Year+City, data=test.frame)
+    data1 <-SantaClaraData2$City
+    data2 <-SantaClaraData2$Year
+    data3<- SantaClaraData2$Population
+    data4<- SantaClaraData2$propertyCrime
+    data5<-SantaClaraData2$UnemploymentRate
+    alldata <- data.frame (data1,data2,data3,data4,data5)
+    test.frame1 <- alldata
+    test.frame2 <- alldata
+    test.frame3 <- alldata
+    test.frame1 <- data.frame(City= input$city, Year= 2012:2016, Population=0)
+    test.frame2 <-data.frame(City= input$city, Year= 2012:2016, propertyCrime=0)
+    test.frame3 <-data.frame(City= input$city, Year= 2012:2016, UnemploymentRate=0)
+    population.frame1 <- predict.lm(mod1,test.frame1)
+    propertyCrime.frame2 <- predict.lm(mod2,test.frame2)
+    UnemploymentRate.frame3 <- predict.lm(mod3,test.frame3)
+    data <- data.frame (City = input$city, year = 2012:2016, Population = population.frame1[1:5], propertyCrime = propertyCrime.frame2[1:5], UnemploymentRate = UnemploymentRate.frame3[1:5])
+    write.csv(data, file ="prediction.csv")
+    test<-read.csv("prediction.csv")
+    gvisMotionChart(test, idvar = "City", timevar = "year")
   })
   
 
--- visualization/ui.r
+++ visualization/ui.r
@@ -4,18 +4,29 @@ library(shiny)
 shinyUI(div(style="background-image: url('http://2.bp.blogspot.com/_oigT0GOX_oY/TUaTLM1xBlI/AAAAAAAAAXs/i6atDqKiSM0/s1600/The-best-top-desktop-blue-wallpapers-blue-wallpaper-blue-background-hd-6.jpg')",pageWithSidebar(
   
   # Application title
-  headerPanel("Visualize Data of Santa Clara County !"),
+  div(style = "text-align:center;color:white",headerPanel("Visualize Data of Santa Clara County !")),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
-    tags$head(
-      tags$style(type="text/css", "select { max-width: 200px; }"),
-      tags$style(type="text/css", "textarea { max-width: 185px; }"),
-      tags$style(type="text/css", ".jslider { max-width: 200px; }"),
-      tags$style(type='text/css', ".well { max-width: 310px;background-color: re }"),
-      tags$style(type='text/css', ".span4 { max-width: 310px; }")
-    ),
-    selectInput("city1", 
+    selectInput("Choice",
+                "Select your Choice",
+                list("Visualize" = 1,
+                     "Predict" = 2)
+                ),
+    conditionalPanel(condition<-"input.Choice==2",selectInput("city", 
+                                                              "Select City",
+                                                              list(
+                                                                "Campbell" = "Campbell",
+                                                                "Gilroy" = "Gilroy", 
+                                                                "Los Altos" = "Los Altos",
+                                                                "Los Gatos" = "Los Gatos",
+                                                                "Milpitas" = "Milpitas",
+                                                                "Morgan Hill" = "Morgan Hill",
+                                                                "Mountain View" = "Mountain View",
+                                                                "Santa Clara" = "Santa Clara",
+                                                                "San Jose" = "San Jose",
+                                                                "Sunnyvale" = "Sunnyvale"))),
+    conditionalPanel(condition<-"input.Choice==1",selectInput("city1", 
                 "Select City 1",
                 list(
                   "Campbell" = "Campbell",
@@ -27,9 +38,9 @@ shinyUI(div(style="background-image: url('http://2.bp.blogspot.com/_oigT0GOX_oY/
                   "Mountain View" = "Mountain View",
                   "Santa Clara" = "Santa Clara",
                   "San Jose" = "San Jose",
-                  "Sunnyvale" = "Sunnyvale")),
+                  "Sunnyvale" = "Sunnyvale"))),
     
-    selectInput("city2", 
+    conditionalPanel(condition<-"input.Choice==1",selectInput("city2", 
                 "Select City 2",
                 list(
                   
@@ -42,16 +53,16 @@ shinyUI(div(style="background-image: url('http://2.bp.blogspot.com/_oigT0GOX_oY/
                   "Santa Clara" = "Santa Clara",
                   "San Jose" = "San Jose",
                   "Sunnyvale" = "Sunnyvale",
-                  "Campbell" = "Campbell")),
-    helpText("Note:All the data collected are real data from FBI websites."),
+                  "Campbell" = "Campbell"))),
+    helpText("Note:All the data collected are real data from FBI websites.")
     
-    submitButton("Update View")
+    #submitButton("Update View")
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
-    p(textOutput("caption")),
-    htmlOutput("gvis")
-   
-  )
+    conditionalPanel(condition<-"input.Choice==1",div(style="color:white",p(textOutput("caption")),htmlOutput("gvis"))),
+    conditionalPanel(condition<-"input.Choice==2", htmlOutput("gvis1"))
+                
+ )
 )))
